4d8a8873d525683b7d5ba374c13b0a6ab97295c4
Nitintembhare29 <115885821+Nitintembhare29@users.noreply.github.com>
2023-04-14 14:54:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const paraElement = document.querySelector("status");
+const EnterBtn = document.querySelector("enterBtn");
+
+EnterBtn.addEventListener("click",(event)=>{
+  paraElement.innerHTML = "<h1>Entered Metaverse</h1>"
+})
